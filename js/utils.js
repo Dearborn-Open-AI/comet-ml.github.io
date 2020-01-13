6921e15bd24cf552dd1fc284b160d886f5a1c90b
Doug Blank <doug.blank@gmail.com>
2020-01-13 09:21:29 -0500

    Add copy() to utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,24 @@
+function copy(obj) {
+  if (!obj || true == obj)
+    return obj;
+  let objType = typeof obj;
+  // Add additional immutables here:
+  if ("number" == objType || "string" == objType)
+    return obj;
+  let result = Array.isArray(obj)
+    ? []
+    : !obj.constructor
+    ? {}
+    : new obj.constructor();
+  if (obj instanceof Map)
+    for (var key of obj.keys()) result.set(key, copy(obj.get(key)));
+  for (var key in obj)
+    if (obj.hasOwnProperty(key)) result[key] = copy(obj[key]);
+  return result;
+}
+
 function compare(field, reverse = false) {
-  //console.log("reverse", reverse);
   return function(a, b) {
-    //console.log("types:", typeof a[field], typeof b[field]);
     let nameA = a[field];
     let nameB = b[field];
     if (typeof a[field] === "string" && typeof b[field] === "string") {
